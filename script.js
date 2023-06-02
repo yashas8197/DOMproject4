fbdff7f6e09bc480ebb7e3ae5a5d5498198babf7
yashas <yashasjr7@gmail.com>
2023-06-02 11:58:40 +0530

    modefied script

--- script.js
+++ script.js
@@ -7,6 +7,17 @@ let seconds = 0;
 let minutes = 0;
 let hours = 0;
 
+//leading variables for zero
+
+let leadingSeconds = 0;
+let leadingMinutes = 0;
+let leadingHours = 0;
+
+// variables for setInterval and timer status
+
+let timerInterval = null;
+let timerStatus = "stopped";
+
 //function for stop watch
 
 function stopWatch(){
@@ -19,8 +30,54 @@ if(seconds / 60 === 1){
         minutes = 0;
     }
 }
+    if(seconds < 10){
+        leadingSeconds = "0" + seconds.toString();
+    } else {
+        leadingSeconds = seconds;
+    }
+
+    if(minutes < 10){
+        leadingMinutes = "0" + minutes.toString();
+    } else {
+        leadingMinutes = minutes;
+    }
+
+    if(hours < 10){
+        leadingHours = "0" + hours.toString();
+    } else {
+        leadingHours = hours;
+    }
+
+
 let displayTimer = document.querySelector('#timer')
-displayTimer.innerHTML = hours + ":" + minutes + ":" + seconds;
+displayTimer.innerHTML = leadingHours + ":" + leadingMinutes + ":" + leadingSeconds;
 }
 
-window.setInterval(stopWatch , 1000);
+// window.setInterval(stopWatch , 1);
+
+let startStopBtn = document.querySelector('#startStopBtn');
+
+startStopBtn.addEventListener('click', () => {
+    if(timerStatus === "stopped"){
+        timerInterval = window.setInterval(stopWatch, 1000);
+        document.getElementById('startStopBtn').innerHTML = `<i class="fa fa-pause" aria-hidden="true" id="pause"></i>`
+        timerStatus = "started"
+    }
+    else {
+        window.clearInterval(timerInterval);
+        document.getElementById('startStopBtn').innerHTML = `<i class="fa fa-play" aria-hidden="true" id="play"></i>`
+        timerStatus = "stopped"
+
+    }
+});
+
+resetBtn.addEventListener('click' ,() => {
+    window.clearInterval(timerInterval);
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    document.querySelector('#startStopBtn').innerHTML = `<i class="fa fa-play" aria-hidden="true" id="play"></i>`
+    document.querySelector('#timer').innerHTML = "00:00:00"
+
+})
+
